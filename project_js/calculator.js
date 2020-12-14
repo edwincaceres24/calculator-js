104e86eaf650de85a1a8157f91df15d7de39d4e3
edwincaceres24 <edwin.caceres@pucp.pe>
2020-12-13 20:31:46 -0500

    Arreglos de funciones

--- project_js/calculator.js
+++ project_js/calculator.js
@@ -1,10 +1,41 @@
+//DOM Operants
+const operatorbutton_add = document.getElementById("add")
+const operatorbutton_substract = document.getElementById("substract")
+const operatorbutton_multiply = document.getElementById("multiply")
+const operatorbutton_divide = document.getElementById("divide")
 
 
-var number;
-function expected_result(number) {
-    number = prompt('Coloca un número ')
-    document.getElementById("your_result").innerHTML=number;
-    console.log(number)
+//Operants selectors
+
+function firstvalue(first_operant) {
+    first_operant=parseInt(prompt('Elige tu 1er número' ));
+    return first_operant
+}
+function secondvalue(second_operant) {
+    second_operant=parseInt(prompt('Elige tu 2do número' ));
+    //Return para arrojar un resultado
+    return second_operant
 }
 
-expected_result(number)
+
+//Operators
+function operator_add(A,B) {
+    document.getElementById("your_result").innerHTML=(firstvalue(A)+secondvalue(B));
+    console.log(A+B)
+}
+function operator_substract(A,B) {
+    document.getElementById("your_result").innerHTML=(firstvalue(A)-secondvalue(B));
+    console.log(A+B)
+}
+function operator_multiply(A,B) {
+    document.getElementById("your_result").innerHTML=(firstvalue(A)*secondvalue(B));
+    console.log(A+B)
+}
+function operator_divide(A,B) {
+    document.getElementById("your_result").innerHTML=(firstvalue(A)/secondvalue(B));
+    console.log(A+B)
+}
+
+
+//Triggers
+operatorbutton_add.addEventListener("click",operator_add)
